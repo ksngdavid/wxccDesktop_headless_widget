cbc6f483a4065389d1954e454f4f31d11a61731f
ksngdavid <ksngdavid@gmail.com>
2024-02-22 22:57:31 +0800

    revert to original

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5777;
 
 //needed for CommonJS
 import { fileURLToPath } from 'url';
